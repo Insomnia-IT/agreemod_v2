39a460888c641aadece1fd058e39431e22b713b2
megorlova <megorlova@mail.ru>
2024-03-27 18:01:52 +0300

    Добавление текущих изменений из мастера в ветку

--- app/routers/people.py
+++ app/routers/people.py
@@ -6,6 +6,11 @@ from app.documenters import Q
 from app.models.person import Person
 from app.schemas.person import PersonFiltersDTO, PersonResponseSchema
 
+<<<<<<< HEAD
+=======
+from app.models.participation import Participation
+from app.db.repos.participation import ParticipationRepo
+>>>>>>> 1706c4c (Эндпоинт и репозиторий для участия)
 
 router = APIRouter()
 
@@ -48,3 +53,15 @@ async def get_orgs_and_volunteers(
     repo: PersonRepo = Depends(get_sqla_repo(PersonRepo)),
 ):
     return await repo.retrieve_many(filters, order_by, limit, offset)
+
+@router.get(
+    "/participation",
+    summary="Участие",
+    response_model=list[Participation],
+)
+async def get_participation(
+    repo: ParticipationRepo = Depends(get_sqla_repo(ParticipationRepo)),
+    page: int = Q("page", 1, description="page"),
+    page_size: int = Q("page size", 10, description="page_size"),
+):
+    return await repo.retrieve_all(page=page, page_size=page_size)
