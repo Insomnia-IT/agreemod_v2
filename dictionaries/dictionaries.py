3dff0bcc54106e39029def77312327d84cde78b1
Alexey Serikov <serikov.alexey42@gmail.com>
2024-06-01 18:28:12 +0300

    add method to define dictionaries enums

---
+++ dictionaries/dictionaries.py
@@ -0,0 +1,44 @@
+from enum import StrEnum
+import logging
+
+import psycopg2
+
+from database.config import config
+
+logger = logging.getLogger(__name__)
+
+try:
+    conn = psycopg2.connect(
+        database="agreemod",
+        user=config.postgres.user,
+        password=config.postgres.password,
+        host=config.postgres.host,
+        port=config.postgres.port,
+    )
+    cur = conn.cursor()
+    cur.execute(f"SELECT code, color FROM public.badge_color")
+    badge_colors = cur.fetchall()
+    # cur.execute(f"SELECT code, diet FROM public.diet_type")
+    # diet_types = cur.fetchall()
+    cur.execute(f"SELECT code, name FROM public.direction_type")
+    direction_types = cur.fetchall()
+    # cur.execute(f"SELECT code, feed FROM public.feed_type")
+    # feed_types = cur.fetchall()
+    # cur.execute(f"SELECT code, gender FROM public.gender")
+    # genders = cur.fetchall()
+    cur.execute(f"SELECT code, name FROM public.participation_role")
+    participation_roles = cur.fetchall()
+    cur.execute(f"SELECT code, name FROM public.participation_status")
+    participation_statuses = cur.fetchall()
+    cur.execute(f"SELECT code, name FROM public.participation_type")
+    participation_types = cur.fetchall()
+    # cur.execute(f"SELECT code, transport FROM public.transport_type")
+    # transport_types = cur.fetchall()
+
+    BadgeColor = StrEnum('BadgeColor', badge_colors)
+    DirectionType = StrEnum('DirectionType', direction_types)
+    ParticipationRole = StrEnum('ParticipationRole', participation_roles)
+    ParticipationStatus = StrEnum('ParticipationStatus', participation_statuses)
+    ParticipationType = StrEnum('ParticipationType', participation_types)
+finally:
+    conn.close()
