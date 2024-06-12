790ae50984481709245f021cd73ee8bb03d12b7f
Alexey Serikov <serikov.alexey42@gmail.com>
2024-06-12 12:02:20 +0300

    fix web paths

--- app/main.py
+++ app/main.py
@@ -39,8 +39,8 @@ def get_app() -> FastAPI:
     )  # TODO: это не подхватывается рефакторингом pycharm
     venusian.Scanner().scan(__import__("app"))
 
-    docs_url = f"{config.API_PREFIX}/_docs"  # if config.DEBUG else None
-    redoc_url = f"{config.API_PREFIX}/_redoc"  # if config.DEBUG else None
+    docs_url = f"/_docs"  # if config.DEBUG else None
+    redoc_url = f"/_redoc"  # if config.DEBUG else None
 
     logger.info(f"doc url: {docs_url}")
     logger.info(f"dev doc url: http://0.0.0.0:8000{docs_url}")
@@ -52,6 +52,7 @@ def get_app() -> FastAPI:
         redoc_url=redoc_url,
         version=config.version,
         description=config.DESCRIPTION,
+        root_path=config.API_PREFIX
     )
 
     # todo:
