6eaf23119d1aaa984a0185d14effd5a256765792
Alexey Serikov <serikov.alexey42@gmail.com>
2024-07-06 12:39:12 +0300

    add background interlock

--- app/services/feeder.py
+++ app/services/feeder.py
@@ -2,6 +2,7 @@ import logging
 
 from datetime import date, datetime, time
 from enum import Enum
+import os
 from typing import Any
 from uuid import UUID
 
@@ -9,6 +10,7 @@ import asyncpg
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_exponential
 
 from app.coda.writer import CodaWriter
 from app.config import config
@@ -26,6 +28,7 @@ from app.schemas.feeder.engagement import EngagementResponse
 from app.schemas.feeder.person import PersonResponse
 from app.schemas.feeder.requests import BackSyncIntakeSchema, SyncResponseSchema
 from app.services.badge_to_notion import notion_writer_v2
+from app.utils.background_tasks import BackgroungInterlock
 
 logger = logging.getLogger(__name__)
 
@@ -50,6 +53,8 @@ def serialize(data: dict) -> dict:
 
     return {x: adapt_to_serialize(y) for x, y in data.items()}
 
+class WaitForItError(Exception):
+    pass
 
 class FeederService:
     def __init__(self, session: AsyncSession) -> None:
@@ -63,101 +68,111 @@ class FeederService:
         self.coda_writer = CodaWriter(api_key=config.coda.api_key, doc_id=config.coda.doc_id)
 
     async def back_sync_badges(self, intake: BackSyncIntakeSchema):
-        badges = intake.badges
-        badges_to_upd = []
-        if badges:
-            existing = await self.badges.update_feeder([x.data for x in badges])
-            for e, b in zip(existing, badges):
-                actor = await self.badges.retrieve(b.actor_badge)
-                dt = b.date.replace(tzinfo=None)
-                await self.logs.add_log(
-                    Logs(
-                        author=actor.name if actor else "ANON",
-                        table_name="badge",
-                        row_id=b.data.id if e else None,
-                        operation="MERGE" if e else "INSERT" if e is not None else "DELETE",
-                        timestamp=dt,
-                        new_data=serialize(b.data.model_dump() if e is not None else {}),
+        async with BackgroungInterlock('back_sync_badges'):
+            badges = intake.badges
+            badges_to_upd = []
+            if badges:
+                existing = await self.badges.update_feeder([x.data for x in badges])
+                for e, b in zip(existing, badges):
+                    actor = await self.badges.retrieve(b.actor_badge)
+                    dt = b.date.replace(tzinfo=None)
+                    await self.logs.add_log(
+                        Logs(
+                            author=actor.name if actor else "ANON",
+                            table_name="badge",
+                            row_id=b.data.id if e else None,
+                            operation="MERGE" if e else "INSERT" if e is not None else "DELETE",
+                            timestamp=dt,
+                            new_data=serialize(b.data.model_dump() if e is not None else {}),
+                        )
                     )
-                )
-                if e is not None:
-                    badges_to_upd.append(b.data.id)
-            await notion_writer_v2(badges_to_upd)
-        await self.session.commit()
+                    if e is not None:
+                        badges_to_upd.append(b.data.id)
+                await notion_writer_v2(badges_to_upd)
+            await self.session.commit()
 
     async def back_sync_arrivals(self, intake: BackSyncIntakeSchema):
-        arrivals = intake.arrivals
-        if arrivals:
-            created, deleted = await self.arrivals.update_feeder([x.data for x in arrivals])
-            for e, a in zip(created, arrivals):
-                actor = await self.badges.retrieve(a.actor_badge)
-                dt = a.date.replace(tzinfo=None)
-                await self.logs.add_log(
-                    Logs(
-                        author=actor.name if actor else "ANON",
-                        table_name="arrival",
-                        row_id=a.data.id if e and e.coda_index is not None else None,
-                        operation="MERGE" if e and e.coda_index is not None else "INSERT" if e is not None else "DELETE",
-                        timestamp=dt,
-                        new_data=serialize(a.data.model_dump() if e is not None else {}),
+        async with BackgroungInterlock('back_sync_arrivals'):
+            arrivals = intake.arrivals
+            if arrivals:
+                created, deleted = await self.arrivals.update_feeder([x.data for x in arrivals])
+                for e, a in zip(created, arrivals):
+                    actor = await self.badges.retrieve(a.actor_badge)
+                    dt = a.date.replace(tzinfo=None)
+                    await self.logs.add_log(
+                        Logs(
+                            author=actor.name if actor else "ANON",
+                            table_name="arrival",
+                            row_id=a.data.id if e and e.coda_index is not None else None,
+                            operation="MERGE" if e and e.coda_index is not None else "INSERT" if e is not None else "DELETE",
+                            timestamp=dt,
+                            new_data=serialize(a.data.model_dump() if e is not None else {}),
+                        )
                     )
-                )
-                if e:
-                    coda_index = await self.coda_writer.update_arrival(self.arrivals, a.data)
-                    e.coda_index = coda_index
-                    await self.session.merge(e)
-            for d in deleted:
-                self.coda_writer.delete_arrival(d)
+                    if e:
+                        coda_index = await self.coda_writer.update_arrival(self.arrivals, a.data)
+                        e.coda_index = coda_index
+                        await self.session.merge(e)
+                for d in deleted:
+                    self.coda_writer.delete_arrival(d)
 
-        await self.session.commit()
+            await self.session.commit()
 
 
     async def back_sync(self, intake: BackSyncIntakeSchema):
-        arrivals = intake.arrivals
-        badges = intake.badges
-        if badges:
-            existing = await self.badges.update_feeder([x.data for x in badges])
-            for e, b in zip(existing, badges):
-                actor = await self.badges.retrieve(b.actor_badge)
-                dt = b.date.replace(tzinfo=None)
-                await self.logs.add_log(
-                    Logs(
-                        author=actor.name if actor else "ANON",
-                        table_name="badge",
-                        row_id=b.data.id if e else None,
-                        operation="MERGE" if e else "INSERT" if e is not None else "DELETE",
-                        timestamp=dt,
-                        new_data=serialize(b.data.model_dump() if e is not None else {}),
+        async with BackgroungInterlock('back_sync'):
+            arrivals = intake.arrivals
+            badges = intake.badges
+            if badges:
+                existing = await self.badges.update_feeder([x.data for x in badges])
+                for e, b in zip(existing, badges):
+                    actor = await self.badges.retrieve(b.actor_badge)
+                    dt = b.date.replace(tzinfo=None)
+                    await self.logs.add_log(
+                        Logs(
+                            author=actor.name if actor else "ANON",
+                            table_name="badge",
+                            row_id=b.data.id if e else None,
+                            operation="MERGE" if e else "INSERT" if e is not None else "DELETE",
+                            timestamp=dt,
+                            new_data=serialize(b.data.model_dump() if e is not None else {}),
+                        )
                     )
-                )
-            badges_uuid = [i.actor_badge for i in badges]
-            await notion_writer_v2(badges_uuid)
+                badges_uuid = [i.actor_badge for i in badges]
+                await notion_writer_v2(badges_uuid)
 
-        if arrivals:
-            created, deleted = await self.arrivals.update_feeder([x.data for x in arrivals])
-            for e, a in zip(created, arrivals):
-                actor = await self.badges.retrieve(a.actor_badge)
-                dt = a.date.replace(tzinfo=None)
-                await self.logs.add_log(
-                    Logs(
-                        author=actor.name if actor else "ANON",
-                        table_name="arrival",
-                        row_id=a.data.id if e and e.coda_index is not None else None,
-                        operation="MERGE" if e and e.coda_index is not None else "INSERT" if e is not None else "DELETE",
-                        timestamp=dt,
-                        new_data=serialize(a.data.model_dump() if e is not None else {}),
+            if arrivals:
+                created, deleted = await self.arrivals.update_feeder([x.data for x in arrivals])
+                for e, a in zip(created, arrivals):
+                    actor = await self.badges.retrieve(a.actor_badge)
+                    dt = a.date.replace(tzinfo=None)
+                    await self.logs.add_log(
+                        Logs(
+                            author=actor.name if actor else "ANON",
+                            table_name="arrival",
+                            row_id=a.data.id if e and e.coda_index is not None else None,
+                            operation="MERGE" if e and e.coda_index is not None else "INSERT" if e is not None else "DELETE",
+                            timestamp=dt,
+                            new_data=serialize(a.data.model_dump() if e is not None else {}),
+                        )
                     )
-                )
-                if e:
-                    coda_index = await self.coda_writer.update_arrival(self.arrivals, a.data)
-                    e.coda_index = coda_index
-                    await self.session.merge(e)
-            for d in deleted:
-                self.coda_writer.delete_arrival(d)
+                    if e:
+                        coda_index = await self.coda_writer.update_arrival(self.arrivals, a.data)
+                        e.coda_index = coda_index
+                        await self.session.merge(e)
+                for d in deleted:
+                    self.coda_writer.delete_arrival(d)
 
-        await self.session.commit()
+            await self.session.commit()
 
+    @retry(
+            stop=stop_after_attempt(5),
+            wait=wait_exponential(multiplier=1, min=4, max=10),
+            retry_if_exception_type=WaitForItError
+        )
     async def sync(self, from_date: datetime):
+        if os.path.isfile('__tasks__/back_sync_badges') or os.path.isfile('__tasks__/back_sync_arrivals') or os.path.isfile('__tasks__/back_sync'):
+            raise WaitForItError()
         get_badges = await self.badges.retrieve_many(include_infant=True, include_directions=True, from_date=from_date)
         badges = [BadgeResponse.model_validate(x.model_dump()) for x in get_badges]
         get_arrivals = await self.arrivals.retrieve_all(from_date=from_date)
